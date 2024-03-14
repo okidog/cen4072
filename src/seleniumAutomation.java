3a5cadb19ce13b173d6276a3b10f0bad75c62677
okikohaku <gtracer465@gmail.com>
2024-03-14 08:59:49 -0400

    Automate dropdown

--- src/seleniumAutomation.java
+++ src/seleniumAutomation.java
@@ -1,8 +1,10 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.JavascriptException;
+import org.openqa.selenium.support.ui.Select;
+
 import java.io.IOException;
 
 public class seleniumAutomation {
@@ -28,6 +30,11 @@ public class seleniumAutomation {
         foxDriver.findElement(By.id("female")).click();
         foxDriver.findElement(By.className("Automation")).click();
 
+        // Init var for dropdown, select text
+        WebElement dropdown = foxDriver.findElement(By.id("testingDropdown"));
+        Select dd = new Select(dropdown);
+        dd.selectByIndex(2);
+        Thread.sleep(500);
 
         Thread.sleep(3000);
         foxDriver.quit(); // End program
